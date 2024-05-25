bc9899a0e97f9a4b4bd308ea298d88a0eb8c430f
Rohit Shrivastava <rohit.shri2389@gmail.com>
2024-05-26 01:04:25 +0530

    lms init

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# lms-next14-prisma
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+
+replace keys in .env
+```
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=###########################################
+CLERK_SECRET_KEY=################################################
+```
+
+Demo [https://lms-next14-nu.vercel.app/](deployed on vercel)
+
---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ToastProvider } from "@/components/providers/toaster-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "LMS application",
+  description: "Online learning plateform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
